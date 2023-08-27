5d998fd99eea024507bf69fc79275f037a9bd207
Drew Gallagher <drewg2009@gmail.com>
2023-08-27 00:32:52 -0300

    added prints to describe things better

--- squaring numbers.py
+++ squaring numbers.py
@@ -10,10 +10,12 @@ if __name__ == '__main__':
         start = time.time()
         p.imap(f, range(1,1000000))
         end = time.time()
+        print("Time to square 1000000 numbers with multiprocessing")
         print(end - start)
 
     start = time.time()
     for val in range(1, 1000000):
         f(val)
     end = time.time()
+    print("Time to square 1000000 numbers with a for loop")
     print(end - start)
